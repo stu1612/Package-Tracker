8316ee35ad300d791b4e9fb64fb3b74ad6108fa8
Stu Bolderson <stu.bolderson@aol.com>
2022-03-22 20:38:45 +0100

    jsx data rendering

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [data, setData] = useState([]);
   const url = "https://my.api.mockaroo.com/insta-orders.json?key=e49e6840";
 
   useEffect(() => {
@@ -10,12 +11,20 @@ export default function Home() {
   async function fetchData(url) {
     const response = await fetch(url);
     const jsonData = await response.json();
-    console.log(jsonData);
+    return setData(jsonData);
   }
+
+  const packages = data.map((item) => (
+    <div key={item.id}>
+      <p>{item.status}</p>
+      <p>{item.id}</p>
+    </div>
+  ));
 
   return (
     <div>
       <h1>Home</h1>
+      {packages}
     </div>
   );
 }
