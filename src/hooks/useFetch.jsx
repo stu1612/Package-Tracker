ec1e46774014c249d4f852d265ce4a1828e82745
Stu Bolderson <stu.bolderson@aol.com>
2022-03-23 15:19:44 +0100

    try catch for laoding and error handling working

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -3,13 +3,21 @@ import { useEffect, useState } from "react";
 export default function useFetch(url) {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     async function fetchData() {
-      const response = await fetch(url);
-      const json = await response.json();
-      setLoading(false);
-      setData(json);
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        setLoading(false);
+        setData(json);
+        setError(null);
+      } catch (err) {
+        setLoading(false);
+        setError("Could not fetch data");
+        console.log(err.message);
+      }
     }
     fetchData();
   }, [url]);
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ export default function Home() {
   const [url, setUrl] = useState(
     "https://my.api.mockaroo.com/insta-orders.json?key=e49e6840"
   );
-  const { data } = useFetch(url);
+  const { data, loading } = useFetch(url);
   // components
   const packageItems =
     data && data.map((item) => <PackageItem key={item.id} item={item} />);
@@ -18,10 +18,12 @@ export default function Home() {
   //   setData(newList);
   // }
 
+  const isPending = loading && <h2>Loading ..</h2>;
+
   return (
     <div>
+      {isPending}
       {packageItems}
-      <button>Hello</button>
     </div>
   );
 }
