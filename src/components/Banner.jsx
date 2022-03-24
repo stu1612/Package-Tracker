4baaa126fac2416d6ab09ebcd1499c2bca92f019
Stu Bolderson <stu.bolderson@aol.com>
2022-03-24 22:57:59 +0100

    banner completed

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,13 +1,16 @@
-import { Trans } from "react-i18next";
+// npm
+import { useTranslation, Trans } from "react-i18next";
 
 export default function Banner() {
+  const { t, i18n } = useTranslation();
+
   return (
-    <div>
-      <p>
+    <div className="banner-image">
+      <h2>
         <Trans i18nKey="description.part1">
           Hi Carl, track all your orders and packages
         </Trans>
-      </p>
+      </h2>
     </div>
   );
 }
